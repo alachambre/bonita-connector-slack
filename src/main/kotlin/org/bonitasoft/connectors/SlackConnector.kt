1d60a7b16815777d513ada85878137f1a3f535a1
Adrien <adrien.lachambre@bonitasoft.com>
2021-10-18 17:28:23 +0200

    - Bump versions - Apply Kotlin suggestions from Intellij

--- src/main/kotlin/org/bonitasoft/connectors/SlackConnector.kt
+++ src/main/kotlin/org/bonitasoft/connectors/SlackConnector.kt
@@ -1,19 +1,15 @@
 package org.bonitasoft.connectors
 
+import com.slack.api.Slack
+import com.slack.api.methods.request.chat.ChatPostMessageRequest
 import org.bonitasoft.engine.connector.AbstractConnector
+import org.bonitasoft.engine.connector.ConnectorException
 import org.bonitasoft.engine.connector.ConnectorValidationException
 import java.util.logging.Logger
-import com.slack.api.Slack
-import com.slack.api.methods.MethodsClient
-import com.slack.api.methods.request.chat.ChatPostMessageRequest
-import java.io.IOException
-import com.slack.api.methods.SlackApiException
-import org.bonitasoft.engine.connector.ConnectorException
-import com.slack.api.methods.request.chat.ChatPostMessageRequest.ChatPostMessageRequestBuilder
 
-open class SlackConnector : AbstractConnector {
+open class SlackConnector : AbstractConnector() {
 
-    val logger = Logger.getLogger(SlackConnector::class.java.name)
+    private val logger: Logger = Logger.getLogger(SlackConnector::class.java.name)
 
     companion object {
         const val TOKEN_INPUT = "tokenInput"
@@ -22,25 +18,19 @@ open class SlackConnector : AbstractConnector {
         const val TS_OUTPUT = "tsOutput"
     }
 
-    constructor() : super()
-
     override fun validateInputParameters() {
         checkMandatoryStringInput(TOKEN_INPUT)
         checkMandatoryStringInput(ID_INPUT)
         checkMandatoryStringInput(MESSAGE_INPUT)
     }
 
-    fun checkMandatoryStringInput(inputName: String) {
+    private fun checkMandatoryStringInput(inputName: String) {
         val value = getInputParameter(inputName)
-        if (!(value is String)) {
+        if (value !is String) {
             throw ConnectorValidationException(this, "'$inputName' parameter must be a String.")
         } else if (value.isEmpty()) {
             throw ConnectorValidationException(this, "Mandatory parameter '$inputName' is missing.")
         }
-    }
-
-    fun setInputParameter(name: String, value: Any) {
-        super.setInputParameters(mapOf(name to value))
     }
 
     fun getOutputParameter(name: String): Any? {
@@ -49,21 +39,21 @@ open class SlackConnector : AbstractConnector {
 
     public override fun executeBusinessLogic() {
         val client = createSlackClient()
-        val token = getInputParameter(TOKEN_INPUT) as String; // We do not want to log the secret token!
+        val token = getInputParameter(TOKEN_INPUT) as String // We do not want to log the secret token!
         val request = createPostMessageRequest()
         val result = client
             .methods(token)
             .chatPostMessage(request)
-        if (result.isOk()) {
-            setOutputParameter(TS_OUTPUT, result.getTs())
+        if (result.isOk) {
+            setOutputParameter(TS_OUTPUT, result.ts)
         } else {
-            throw ConnectorException("Send slack message failed with the following error: ${result.getError()}")
+            throw ConnectorException("Send slack message failed with the following error: ${result.error}")
         }
     }
     
     fun createPostMessageRequest() : ChatPostMessageRequest {
-        val channel = getAndLogStringParameter(ID_INPUT);
-        val message = getAndLogStringParameter(MESSAGE_INPUT);
+        val channel = getAndLogStringParameter(ID_INPUT)
+        val message = getAndLogStringParameter(MESSAGE_INPUT)
         return ChatPostMessageRequest.builder()
                 .channel(channel)
                 .text(message)
@@ -74,9 +64,9 @@ open class SlackConnector : AbstractConnector {
         return Slack.getInstance()
     }
     
-    public fun getAndLogStringParameter(parameterName : String) : String {
+    private fun getAndLogStringParameter(parameterName : String) : String {
         val value : String = getInputParameter(parameterName) as String
-        logger.info({ -> "$parameterName: $value"})
+        logger.info { "$parameterName: $value" }
         return value
     }
 }
